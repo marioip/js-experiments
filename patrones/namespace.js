4f09ed8cbf77c2fe78713c1112d640505b13854a
Mario Iglesias <marioip@gmail.com>
2013-04-09 11:08:06 +0200

    ejercicios de patrones

---
+++ patrones/namespace.js
@@ -0,0 +1,15 @@
+
+var MiLib = (function (MiLib){
+
+  MiLib.namespace = function(names, fns){
+    var myActions = names.split(".");
+
+  }
+
+})(MiLib || {})
+
+
+MiLib.namespace('widgets.buttons.actionButtons', function(my) {
+  my.ok = new Widget({ ... });
+  my.cancel = new Widget({ ... });
+});
