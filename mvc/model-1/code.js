91972de58372717e582703cabdefd413a3cda92e
Mario Iglesias <marioip@gmail.com>
2013-04-10 22:27:38 +0200

    Ejercicios mvc

---
+++ mvc/model-1/code.js
@@ -0,0 +1,29 @@
+var Producto = ProJS.Model.extend({
+  // escribe aquí
+
+  init: function(tipo){
+  	this.tipo = tipo;
+  	console.log(this.tipo);
+  	this._super;
+  },
+  defaults: {
+  	nombre: "Producto",
+  	categoria: "articulo",
+  	precio: "0"
+  },
+  set: function(attributes, options) {
+    console.log("set");
+  },
+  get: function(){
+  	return this.tipo;
+  },
+  validate: function(attributes){
+  	console.log(attributes);
+  }
+
+});
+
+var p = new Producto("sombrero");
+var atrs = {nombre:"Camiseta",categoria:"camisetas",precio:20};
+p.set(atrs,{validate: true});
+console.log("get"+p.get());
